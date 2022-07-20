f69442ec76e56358c471b21627cccc6a1f38f7c0
Kentaro Maruyama <marukeso@gmail.com>
2022-07-20 15:21:59 +0900

    feat: first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authservice: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authservice.signUp(createUserDto);
+  }
+
+  @Post('signin')
+  async signin(
+    @Body() credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authservice.signIn(credentialsDto);
+  }
+}
